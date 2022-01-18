a5285d6628dbf32d730072fbad747e672e4bc2b8
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2022-01-18 00:24:36 -0500

    clarify requirements, add dep

--- packaging.sh
+++ packaging.sh
@@ -5,17 +5,19 @@
 #The '/opt/qt515/bin/qmake' part can be replaced with just 'qmake' on modern distros.
 #please also refer to https://gitlab.com/CalcProgrammer1/OpenRGB#linux
 
-### GCC
+### GCC (needed to build and to run)
 # sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 # sudo apt install gcc-11 g++-11
+# sudo apt install libgcc-s1
 
-### QT
+### QT (needed just to build, system QT will probably suffice to install/run, I think?)
 # x86 machines: sudo add-apt-repository ppa:beineri/opt-qt-5.15.2-bionic
 # ARM machines: sudo add-apt-repository ppa:theofficialgman/opt-qt-5.15.2-bionic-arm
+# sudo apt install qt515base AndOtherStuffHere
 
 ### LLVM 13
 # https://apt.llvm.org/
-# I have no idea why (or if) this is needed, but it showed up in the .buildinfo for the .deb I made
+# afaik this is not needed to run at all, but it showed up in the .buildinfo for the .deb I made for some reason
 
 rm -rf OpenRGB/
 git clone https://gitlab.com/CalcProgrammer1/OpenRGB
